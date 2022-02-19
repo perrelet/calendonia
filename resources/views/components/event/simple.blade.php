45e2129e2e66b7f1f97b86093cea23222d6ef25f
Jamie <jperrelet@gmail.com>
2022-02-19 12:07:25 -0800

    Simple calendar view outputs

--- resources/views/components/event/simple.blade.php
+++ resources/views/components/event/simple.blade.php
@@ -1,11 +1,17 @@
-<x-event.part.wrap {{ $attributes }} :url="$event->url" tag="article">
+<x-event.part.wrap {{ $attributes->merge(['class' => 'event']) }} :url="$event->url" tag="article">
     <div class="paper-date">
         <div data-period="D">{{ $event->get_start_date("D") }}</div>
         <div data-period="j">{{ $event->get_start_date("j") }}</div>
         <div data-period="M">{{ $event->get_start_date("M") }}</div>
     </div>
-    <div>
-        {{ $event['title'] }}
-        {{ $event->get_start_date() }}
+    <div class="info">
+        <div class="title">{{ $event['title'] }}</div>
+        <div class="time">{{ $event->get_start_date("h:i a") }} <span class='timezone'>{{ $event->get_start_date("T") }}</span></div>
+        <div class="type" data-type="{{ str($event['type'])->slug() }}">{{ $event['type'] }}</div>
+        {{--<div class="tags">
+        @foreach ($event->tags as $tag)
+            <div>{{ $tag->translate('name') }}</div>
+        @endforeach
+        </div>--}}
     </div>
 </x-event.part.wrap>
