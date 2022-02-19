8feada9a9aeeef337302cb5b3b7e12fc194ef523
Jamie <jperrelet@gmail.com>
2022-02-19 12:07:11 -0800

    Merge attributes in events component

--- resources/views/components/events.blade.php
+++ resources/views/components/events.blade.php
@@ -1,3 +1,5 @@
+<div {{ $attributes->merge(['class' => 'events']) }}>
 @foreach ($events as $event)
     <x-dynamic-component {{ $attributes }} :component="$component" :event="$event"/>
 @endforeach
+</div>
