3a122d796cd91012ac823c5655a05de59aa3644e
Jamie <jperrelet@gmail.com>
2022-02-19 12:06:49 -0800

    Basic calendar styles

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -6,15 +6,13 @@
 
         <title>Calendonia</title>
 
+        <link href='{{ url("/css/normalize.css") }}' rel='stylesheet'>
         <link href='{{ url("/css/{$template}.css") }}' rel='stylesheet'>
 
-        <!--<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet"> -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
     </head>
     <body>
         <x-events :component="$component" :events="$events"/>
