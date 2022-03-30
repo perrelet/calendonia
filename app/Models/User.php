ba35d6fee3e0235d8dc40c68595a7d58a92a1abd
Jamie <jperrelet@gmail.com>
2022-03-30 12:09:31 -0700

    Filament user setup

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -41,4 +42,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function canAccessFilament(): bool
+    {
+
+        $allowed_emails = ['jperrelet@gmail.com'];
+
+        return in_array($this->email, $allowed_emails) && $this->hasVerifiedEmail();
+
+        //return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+    }
+
 }
