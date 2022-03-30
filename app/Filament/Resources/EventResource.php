9e5dbcaae4e2bdadf77f9ca875df8327a41b2cb3
Jamie <jperrelet@gmail.com>
2022-03-30 15:57:39 -0700

    Change menu icon

--- app/Filament/Resources/EventResource.php
+++ app/Filament/Resources/EventResource.php
@@ -16,7 +16,7 @@ class EventResource extends Resource
 {
     protected static ?string $model = Event::class;
     protected static ?string $recordTitleAttribute = 'id';
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-calendar';
 
     public static function canDelete(Model $record): bool
     {
@@ -55,6 +55,7 @@ class EventResource extends Resource
             'index' => Pages\ListEvents::route('/'),
             'create' => Pages\CreateEvent::route('/create'),
             'edit' => Pages\EditEvent::route('/{record}/edit'),
+            'view' => Pages\ViewEvent::route('/{record}/view'),
         ];
     }
 }
