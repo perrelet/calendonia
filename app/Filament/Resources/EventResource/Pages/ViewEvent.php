0e0b3a5bfb0eed9a7ca234e7070f2f502a2d5eee
Jamie <jperrelet@gmail.com>
2022-03-30 19:29:46 -0700

    Config ViewEvent view

--- app/Filament/Resources/EventResource/Pages/ViewEvent.php
+++ app/Filament/Resources/EventResource/Pages/ViewEvent.php
@@ -3,11 +3,24 @@
 namespace App\Filament\Resources\EventResource\Pages;
 
 use App\Filament\Resources\EventResource;
-use Filament\Resources\Pages\Page;
+use Filament\Resources\Pages\ViewRecord;
 
-class ViewEvent extends Page
+class ViewEvent extends ViewRecord
 {
     protected static string $resource = EventResource::class;
 
-    protected static string $view = 'filament.resources.event-resource.pages.view-event';
+    //protected static string $view = 'filament.resources.event-resource.pages.view-event';
+
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->makeForm()
+                ->disabled()
+                ->model($this->record)
+                ->schema($this->getResourceForm(columns: config('filament.layout.forms.have_inline_labels') ? 1 : 2)->getSchema())
+                ->statePath('data')
+                ->inlineLabel(true)
+        ];
+    }
+    
 }
