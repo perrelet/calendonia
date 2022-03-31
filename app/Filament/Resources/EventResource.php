334e86e6a1cdb2152b96eb4cafe928a7a5270f31
Jamie <jperrelet@gmail.com>
2022-03-30 19:30:21 -0700

    Chahe resource title

--- app/Filament/Resources/EventResource.php
+++ app/Filament/Resources/EventResource.php
@@ -15,7 +15,7 @@ use Illuminate\Database\Eloquent\Model;
 class EventResource extends Resource
 {
     protected static ?string $model = Event::class;
-    protected static ?string $recordTitleAttribute = 'id';
+    protected static ?string $recordTitleAttribute = 'title';
     protected static ?string $navigationIcon = 'heroicon-o-calendar';
 
     public static function canDelete(Model $record): bool
@@ -25,17 +25,34 @@ class EventResource extends Resource
 
     public static function form(Form $form): Form
     {
+
         return $form
             ->schema([
-                //
-            ]);
+                Forms\Components\TextInput::make('id'),
+                Forms\Components\TextInput::make('title'),
+                Forms\Components\TextInput::make('sub_title'),
+                Forms\Components\TextInput::make('type'),
+                Forms\Components\TextInput::make('importance'),
+                Forms\Components\TextInput::make('connection_id'),
+                Forms\Components\TextInput::make('external_id'),
+                Forms\Components\TextInput::make('url'),
+                Forms\Components\TextInput::make('start_date'),
+                Forms\Components\TextInput::make('end_date'),
+                Forms\Components\TextInput::make('image'),
+                Forms\Components\TextInput::make('thumb'),
+                Forms\Components\TextInput::make('virtual'),
+                Forms\Components\TextInput::make('price'),
+                Forms\Components\TextInput::make('currency'),
+                Forms\Components\TextInput::make('created_at'),
+                Forms\Components\TextInput::make('updated_at'),
+            ])
+            ->columns(1);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
             ])
             ->filters([
                 //
@@ -55,7 +72,7 @@ class EventResource extends Resource
             'index' => Pages\ListEvents::route('/'),
             'create' => Pages\CreateEvent::route('/create'),
             'edit' => Pages\EditEvent::route('/{record}/edit'),
-            'view' => Pages\ViewEvent::route('/{record}/view'),
+            /* 'view' => Pages\ViewEvent::route('/{record}'), */
         ];
     }
 }
