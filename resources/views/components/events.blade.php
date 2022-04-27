af15f0333b1ebf84b1f01c4e764987ecd5ef7569
Jamie <jperrelet@gmail.com>
2022-04-27 21:04:18 +0200

    Group events

--- resources/views/components/events.blade.php
+++ resources/views/components/events.blade.php
@@ -1,5 +1,17 @@
 <div {{ $attributes->merge(['class' => 'events']) }}>
-@foreach ($events as $event)
-    <x-dynamic-component {{ $attributes }} :component="$component" :event="$event"/>
-@endforeach
+@if ($grouped)
+   @foreach ($grouped as $key => $group)
+    <div class='item divider'>{{ $key }}</div>
+    @foreach ($group as $event)
+        <x-dynamic-component {{ $attributes }} :component="$component" :event="$event"/>
+    @endforeach
+    @endforeach
+@else
+    @foreach ($events as $event)
+        <x-dynamic-component {{ $attributes }} :component="$component" :event="$event"/>
+    @endforeach
+@endif
 </div>
+@if ($events instanceof \Illuminate\Pagination\LengthAwarePaginator)
+{{ $events->withQueryString()->links() }}
+@endif
