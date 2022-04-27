96627d890ae6ec6561868d3f874c9568d927d077
Jamie <jperrelet@gmail.com>
2022-04-27 21:01:01 +0200

    Hub view

---
+++ resources/views/components/event/hub.blade.php
@@ -0,0 +1,18 @@
+<x-event.part.wrap {{ $attributes->merge(['class' => 'event']) }} :url="$event->url" tag="article">
+    {{--<div class="thumb" style="background-image: url({{ $event->thumb }});"></div>--}}
+    <div class="paper-date">
+        <div data-period="D">{{ $event->get_start_date("D") }}</div>
+        <div data-period="j">{{ $event->get_start_date("j") }}</div>
+        <div data-period="M">{{ $event->get_start_date("M") }}</div>
+    </div>
+    <div class="info">
+        <div class="title">{{ $event['title'] }}</div>
+        <div class="time">{{ $event->get_start_date("h:i a") }} <span class='timezone'>{{ $event->get_start_date("T") }}</span></div>
+        <div class="type" data-type="{{ str($event['type'])->slug() }}">{{ $event['type'] }}</div>
+        {{--<div class="tags">
+        @foreach ($event->tags as $tag)
+            <div>{{ $tag->translate('name') }}</div>
+        @endforeach
+        </div>--}}
+    </div>
+</x-event.part.wrap>
